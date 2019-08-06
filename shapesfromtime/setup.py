2b777859a246c3ee6173f727b628ff0970c9b359
Rohit Vincent <therohitvincent@gmail.com>
2019-08-06 22:19:50 +0100

    Updated Setup

---
+++ shapesfromtime/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='shapesfromtime',
+      version='0.1',
+      description='Time series classification Using Shapelets',
+      url='https://github.com/rohitvincent/shapes-from-time',
+      author='Rohit Vincent',
+      author_email='rohit.vincent@ucdconnect.ie',
+      license='MIT',
+      packages=['shapesfromtime'],
+      zip_safe=False)
